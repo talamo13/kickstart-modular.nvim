0fee2b2af595b3bfa9308c1575ab0e900532140b
Tyler <tyleralamo15@gmail.com>
2024-11-03 22:49:03 -0800

    Finally got the webdev icons to work

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -23,25 +23,25 @@ require('lazy').setup({
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
 
-  require 'kickstart/plugins/gitsigns',
+  -- require 'kickstart/plugins/gitsigns',
 
-  require 'kickstart/plugins/which-key',
+  -- require 'kickstart/plugins/which-key',
 
-  require 'kickstart/plugins/telescope',
+  -- require 'kickstart/plugins/telescope',
 
-  require 'kickstart/plugins/lspconfig',
+  -- require 'kickstart/plugins/lspconfig',
 
-  require 'kickstart/plugins/conform',
+  -- require 'kickstart/plugins/conform',
 
-  require 'kickstart/plugins/cmp',
+  -- require 'kickstart/plugins/cmp',
 
-  require 'kickstart/plugins/tokyonight',
+  -- require 'kickstart/plugins/tokyonight',
 
-  require 'kickstart/plugins/todo-comments',
+  -- require 'kickstart/plugins/todo-comments',
 
-  require 'kickstart/plugins/mini',
+  -- require 'kickstart/plugins/mini',
 
-  require 'kickstart/plugins/treesitter',
+  -- require 'kickstart/plugins/treesitter',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
