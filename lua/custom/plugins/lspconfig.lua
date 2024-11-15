43b99fd8793ff2c9ab65facd5a4cc28e422d579e
Tyler <tyleralamo15@gmail.com>
2024-11-14 18:09:05 -0800

    fixing css ls name

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -188,7 +188,7 @@ return {
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
         --
-        css_lsp = {},
+        cssls = {},
 
         lua_ls = {
           -- cmd = {...},
